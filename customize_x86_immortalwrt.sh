7d321f2c3c937272b3a03aac9e1c5c73ba195dc2
riches2020cn <141565770+riches2020cn@users.noreply.github.com>
2025-04-08 20:24:53 +0800

    Update customize_x86_immortalwrt.sh

--- customize_x86_immortalwrt.sh
+++ customize_x86_immortalwrt.sh
@@ -7,16 +7,16 @@
 # Description: Build OpenWrt using GitHub Actions
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.9.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.9.1/g' immortalwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
-#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' immortalwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. 修改默认皮肤
-#sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' openwrt/feeds/luci/collections/luci/Makefile
+#sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' immortalwrt/feeds/luci/collections/luci/Makefile
 
 #4. 修改 x86 内核版本
-#sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' openwrt/target/linux/x86/Makefile
+#sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' immortalwrt/target/linux/x86/Makefile
 
 #5. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rf
+#rm immortalwrt/package/lean/luci-theme-argon -rf
